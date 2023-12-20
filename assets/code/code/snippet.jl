b4978bd3192d93e72154f00c94e333a059cec1f6
nathanaelbosch <nathanael.bosch@uni-tuebingen.de>
2023-12-20 13:25:38 +0000

    Deploying to gh-pages from  @ c61470f63f1d0a445736075b726280dcc09fadb1 🚀

---
+++ assets/code/code/snippet.jl
@@ -0,0 +1,32 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using HTTP
+using JSON
+
+function get_github_description(repo)
+    repo_url = "https://github.com/" * repo
+    # Extract owner and repo name from the URL
+    parts = split(strip(repo_url, '/'), '/')
+    owner = parts[end-1]
+    repo_name = parts[end]
+
+    # GitHub API endpoint for repository information
+    api_url = "https://api.github.com/repos/$owner/$repo_name"
+
+    # Make a GET request to the GitHub API
+    headers = Dict("Authorization" => "token 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")
+    response = HTTP.get(api_url, headers=headers)
+
+    if response.status == 200
+        # Parse the JSON response
+        repo_info = JSON.parse(String(response.body))
+
+        # Extract and return the description
+        return get(repo_info, "description", "No description available")
+
+    else
+        return "Error: $(response.status) - Unable to fetch repository information"
+    end
+end
+
+println(get_github_description("nathanaelbosch/PSDMatrices.jl"))
